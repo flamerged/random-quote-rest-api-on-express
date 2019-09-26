fff5b5ad0febd0e42e3822668da388d086a5e081
flamerged <mersad.ajanovic@googlemail.com>
2019-09-26 20:55:17 +0200

    added routes to acces all or single quotes by using the get functions from records.js

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+const records = require('./records');
 
-app.get('/greetings', (req, res) => {
-    res.json({ greeting: 'Hello world' });
+// Actions that we want to enable:
+// Send a GET request to /quotes to READ a list of quotes
+app.get('/quotes', async (req, res) => {
+    const quotes = await records.getQuotes();
+    res.json(quotes);
 });
+
+// Send a GET request to /quotes/:id to READ(view) a quote
+
+app.get('/quotes/:id', async (req, res) => {
+    const quote = await records.getQuote(req.params.id);
+    res.json(quote);
+});
+
+// Send a POST request to /quotes/ to Create a quote
+// Send a PUT request to /quotes/:id to UPDATE(edit) a quote
+// Send a DELETE request to /quotes/:id to DELETE a quote
+// Send a GET request to /quotes/quote/random to READ (view) a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
